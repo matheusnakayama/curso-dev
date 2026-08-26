b4bd87604253fa0d3c269f5d1a74a220b71b7dc7
matheushideo <114515802+matheusnakayama@users.noreply.github.com>
2026-08-26 22:20:51 +0000

    faz um teste para eu mostrar para meu amigo

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 function Home() {
-    return <h1>Teste diferente para o .git push 🐒</h1>
+    return <h1>Teste diferente para o .git push 🐒 
+        junto tem um teste para mostrar ao meu amigo
+    </h1>
 }
 
 export default Home;
